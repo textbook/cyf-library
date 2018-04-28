b4903fa764ce280e6efd912c16ef2bfae6d14fe5
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-04-28 11:42:14 +0100

    Use Bootstrap styling for header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,9 +2,11 @@ import React from 'react'
 import './Header.css'
 
 const Header = () => (
-  <header>
-    <img src="cyf_logo.png" alt="Code Your Future logo"/>
-    <h1 className="page-title d-none d-sm-block">Resource library</h1>
+  <header className="navbar navbar-light justify-content-start">
+    <span className="navbar-brand">
+      <img src="cyf_logo.png" alt="Code Your Future logo"/>
+    </span>
+    <h1 className="page-title navbar-text d-none d-sm-block">Resource library</h1>
   </header>
 )
 
